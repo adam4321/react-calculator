329e8db04e18005fac7d4e84b37fccc888c383e4
Adam Wright <adamjw321@gmail.com>
2018-11-22 15:19:56 -0800

    ts-check

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
+
+//ts-check
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
 
-<div className='display'>0</div>
+state={
+  displayValue: '0'
+};
+
+  render() {
+
+const {displayValue} = this.state
+
+    return (
+      <div className='App'>
+      <div className='display'>{displayValue}</div>
+        
+
+<div ></div>
         
       </div>
     );
