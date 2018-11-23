9bbd2189e66ae972811981d94b68e76690471af0
Adam Wright <adamjw321@gmail.com>
2018-11-22 17:38:44 -0800

    new keys

--- src/App.js
+++ src/App.js
@@ -31,23 +31,23 @@ const {displayValue} = this.state
         <div className='input-keys'>
 
          <div className='function-keys'>
-            <button className='key key-clear'>AC</button>
-            <button className='key key-sign'>&plusmn;</button>
-            <button className='key key-percent'>%</button>
+            <button className='key key-func key-clear'>AC</button>
+            <button className='key key-func key-sign'>&plusmn;</button>
+            <button className='key key-func key-percent'>%</button>
         </div>
 
         <div className='digit-keys'>
-            <button className='key key-0' onClick={() => this.inputDigit(0)}>0</button>
-            <button className='key key-dot'>.</button>
-            <button className='key key-1' onClick={() => this.inputDigit(1)}>1</button>
-            <button className='key key-2' onClick={() => this.inputDigit(2)}>2</button>
-            <button className='key key-3' onClick={() => this.inputDigit(3)}>3</button>
-            <button className='key key-4' onClick={() => this.inputDigit(4)}>4</button>
-            <button className='key key-5' onClick={() => this.inputDigit(5)}>5</button>
-            <button className='key key-6' onClick={() => this.inputDigit(6)}>6</button>
-            <button className='key key-7' onClick={() => this.inputDigit(7)}>7</button>
-            <button className='key key-8' onClick={() => this.inputDigit(8)}>8</button>
-            <button className='key key-9' onClick={() => this.inputDigit(9)}>9</button>
+            <button className='key key-dig key-0' onClick={() => this.inputDigit(0)}>0</button>
+            <button className='key key-dig key-dot'>.</button>
+            <button className='key key-dig key-1' onClick={() => this.inputDigit(1)}>1</button>
+            <button className='key key-dig key-2' onClick={() => this.inputDigit(2)}>2</button>
+            <button className='key key-dig key-3' onClick={() => this.inputDigit(3)}>3</button>
+            <button className='key key-dig key-4' onClick={() => this.inputDigit(4)}>4</button>
+            <button className='key key-dig key-5' onClick={() => this.inputDigit(5)}>5</button>
+            <button className='key key-dig key-6' onClick={() => this.inputDigit(6)}>6</button>
+            <button className='key key-dig key-7' onClick={() => this.inputDigit(7)}>7</button>
+            <button className='key key-dig key-8' onClick={() => this.inputDigit(8)}>8</button>
+            <button className='key key-dig key-9' onClick={() => this.inputDigit(9)}>9</button>
           </div>
 
         <div className='operator-keys'>
