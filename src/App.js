a8a548cc4b4618ee1d00c8e054ab11edff880e00
Wright <wrighada@oregonstate.edu>
2019-11-03 00:20:20 -0700

    split out display component

--- src/App.js
+++ src/App.js
@@ -8,37 +8,9 @@
 
 import React, {Component} from 'react';
 import './App.css';
-import AutoShrinkingText from './AutoShrinkingText.js';
+import CalculatorDisplay from './CalculatorDisplay.js';
 
 
-
-class CalculatorDisplay extends React.Component {
-    render() {
-        const {value, ...props} = this.props;
-        const language = navigator.language || 'en-US';
-        let formattedValue = parseFloat(value).toLocaleString(language, {
-            useGrouping: true,
-            maximumFractionDigits: 6
-        });
-
-        // Add back missing .0 in e.g. 12.0
-        const match = value.match(/\.\d*?(0*)$/);
-
-        if (match)
-            formattedValue += /[1-9]/.test(match[0]) ? match[1] : match[0];
-
-        return (
-            <div {...props} className = "display">
-                <AutoShrinkingText>{formattedValue}</AutoShrinkingText>
-            </div>
-        );
-    }
-}
-
-
-/*************************************************************
-**  Calculator display component
-*************************************************************/
 class App extends Component {
     state = {
         value: null,
