3a2b8339168a5fa966eea7a3418d6251fb5c25de
Adam Wright <adam@adamjwright.com>
2021-01-28 22:35:40 -0800

    update dependencies

--- src/CalculatorDisplay.js
+++ src/CalculatorDisplay.js
@@ -19,8 +19,9 @@ class CalculatorDisplay extends React.Component {
         // Add back missing .0 in e.g. 12.0
         const match = value.match(/\.\d*?(0*)$/);
 
-        if (match)
+        if (match) {
             formattedValue += /[1-9]/.test(match[0]) ? match[1] : match[0];
+        }
 
         return (
             <div {...props} className = "display">
